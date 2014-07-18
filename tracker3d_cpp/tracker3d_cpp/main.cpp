719f37ec22fceaa319a444af0c6388b2ea8dbca2
Ryan Morshead <ryan.morshead@gmail.com>
2014-07-17 18:34:23 -0700

    added translation from hexagonal to cartesian coordinates

--- tracker3d_cpp/tracker3d_cpp/main.cpp
+++ tracker3d_cpp/tracker3d_cpp/main.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  tracker3d_cpp
@@ -8,6 +9,7 @@
 
 #include <iostream>
 #include <istream>
+#include <stdio.h>
 #include <array>
 #include <vector>
 using namespace std;
@@ -18,13 +20,13 @@ using namespace std;
 class Voxel
 {/*The basic data unit; represents pixels in 3 dimensions (two spatial and one temporal) and it's associated data.
     <data_member(abbreviation): description>
-    voxStamp(vst): contains four values corrisponding to the chamber and coordinates of the voxel:
+    voxStamp(stmp): contains four values corrisponding to the chamber and coordinates of the voxel:
         {chamber, row (spatial), collumn(spatial), and bucket(temporal)}.
     acdValue(adc): The adc value is the magnitude of the signal recorded*/
 public:
-    Voxel(int vst[4], int adc)
+    Voxel(int stmp[4], int adc)
     {
-        voxStamp = vst;
+        voxStamp = stmp;
         adcValue = adc;
     }
     
@@ -45,17 +47,14 @@ private:
     int adcValue;
 };
 
-
 class Event
 {
 public:
-    Event(int eventId)
-    {
+    Event(int eventId) {
         Id = eventId;
     }
     
-    void addVoxel(int voxArray[5])
-    {
+    void addVoxel(int voxArray[5]) {
         int stamp[4] = {
             voxArray[0],
             voxArray[1],
@@ -65,7 +64,49 @@ public:
         voxPortfolio.push_back(Voxel(stamp, voxArray[5]));
     }
     
-    
+    //!temporary change!
+    /*vector<Voxel>*/ int getNeighbors(int voxStamp[4])
+    //!temporary change!
+    {
+        int stampNeighbors[80];
+        int *pstmpN= stampNeighbors;
+        
+        int transRule[3] = {-1,0,1};
+        
+        int count=0;
+        //if (stmpV[1]%2 == 1)
+        for(int i=0; i<3; i++) {
+            if (i!=1) {
+                pstmpN[4*count+0] = voxStamp[0];
+                pstmpN[4*count+3] = voxStamp[3]+transRule[i];
+                pstmpN[4*count+1] = voxStamp[1];
+                pstmpN[4*count+2] = voxStamp[2];
+                count+=1;
+            }
+            
+            for(int j=0; j<3; j++) {
+                for(int k=0; k<2; k++) {
+                    pstmpN[4*count+3] = voxStamp[3]+transRule[i];
+                    pstmpN[4*count+1] = voxStamp[1]+transRule[j];
+                    
+                    if (voxStamp[1]%2 == 1) {
+                        pstmpN[4*count+2] = voxStamp[2]+transRule[k+1];
+                    }
+                    else {
+                        pstmpN[4*count+2] = voxStamp[2]+transRule[k];
+                    }
+                    pstmpN[4*count+0] = voxStamp[0];
+                    count+=1;
+                }
+            }
+        }
+        
+        //!temporary change!
+        return 0;
+        //!temporary change!
+    }
+        
+        
     
     
 private:
@@ -83,6 +124,49 @@ private:
 
 int main(int argc, const char * argv[])
 {
+    int voxStamp[4]={3,3,3,3};
+    int stampNeighbors[80];
+    int *pstmpN= stampNeighbors;
+    
+    int transRule[3] = {-1,0,1};
+    
+    int count=0;
+    //if (stmpV[1]%2 == 1)
+    for(int i=0; i<3; i++) {
+        if (i!=1) {
+            pstmpN[4*count+0] = voxStamp[0];
+            pstmpN[4*count+3] = voxStamp[3]+transRule[i];
+            pstmpN[4*count+1] = voxStamp[1];
+            pstmpN[4*count+2] = voxStamp[2];
+            count+=1;
+        }
+        
+        for(int j=0; j<3; j++) {
+            for(int k=0; k<2; k++) {
+                pstmpN[4*count+3] = voxStamp[3]+transRule[i];
+                pstmpN[4*count+1] = voxStamp[1]+transRule[j];
+                
+                if (voxStamp[1]%2 == 1) {
+                    pstmpN[4*count+2] = voxStamp[2]+transRule[k+1];
+                }
+                else {
+                    pstmpN[4*count+2] = voxStamp[2]+transRule[k];
+                }
+                pstmpN[4*count+0] = voxStamp[0];
+                count+=1;
+            }
+        }
+    }
+    
+    for (int i=0; i<20; i++) {
+        for(int j=0; j<4; j++) {
+            cout << pstmpN[4*i+j] << " ";
+            if (j==3) {
+                cout << "\n";
+            }
+        }
+    }
+
 
 }
 
